8f7046a904807967f3742f28ff2669cc104763c0
Sourcery AI <>
2023-12-19 17:57:46 +0000

    'Refactored by Sourcery'

--- 2023/homework/shanleilei/852.py
+++ 2023/homework/shanleilei/852.py
@@ -1,10 +1,10 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        #遍历整个数组
-        for i in range(len(arr)-1, -1,-1):
-            #当索引对应值大于或等于两边时即为“山峰”
-            if arr[i]>=arr[i-1] and arr[i]>=arr[i+1]:
-
-                
-                return i
-        return -1
+        return next(
+            (
+                i
+                for i in range(len(arr) - 1, -1, -1)
+                if arr[i] >= arr[i - 1] and arr[i] >= arr[i + 1]
+            ),
+            -1,
+        )
